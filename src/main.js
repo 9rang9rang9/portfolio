2f1a556d1f8e107eeef692e0b267818028d86f33
9rang9rang9 <badchoi87@gmail.com>
2025-03-09 03:54:23 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -15,3 +15,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener("scroll", () => {
   home.style.opacity = Math.max(1 - window.scrollY / homeHeight, 0);
 });
+
+// Arrow up버튼을 아래로 스크롤시 나타나게 처리함
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
